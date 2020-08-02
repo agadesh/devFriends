3b353eab46d61e18fc6e25f9bb9b8bc59f309169
Adesh Agarwal <adeshagarwal440@gmail.com>
2020-08-03 00:32:15 +0530

    Updates

---
+++ precache-manifest.11f0fc523991df87880cba4e8b005e3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd815bab030557d40e4c241e5ed264c0",
+    "url": "/devFriends/index.html"
+  },
+  {
+    "revision": "3e3bf7a3da8f7f1ac00e",
+    "url": "/devFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7c8026a28e1f125d4c6d",
+    "url": "/devFriends/static/css/main.bf8d8bea.chunk.css"
+  },
+  {
+    "revision": "3e3bf7a3da8f7f1ac00e",
+    "url": "/devFriends/static/js/2.a37f9ec5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/devFriends/static/js/2.a37f9ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8026a28e1f125d4c6d",
+    "url": "/devFriends/static/js/main.6444f9ee.chunk.js"
+  },
+  {
+    "revision": "41a1099e7da99b443adf",
+    "url": "/devFriends/static/js/runtime-main.40a96661.js"
+  },
+  {
+    "revision": "efcd235d050134046cbd2516c83b6b18",
+    "url": "/devFriends/static/media/Fattern.efcd235d.woff"
+  }
+]);
